58a58254dc766253ae63e7dc8f9bac8470b3eaa8
abbrat <abhaysinghpat12@gmail.com>
2019-11-01 09:07:47 +0530

    deleted addtocar js and modified prjs

--- prjs.js
+++ prjs.js
@@ -142,14 +142,14 @@ function addtojson()
       alert("desc cant be left empty");
 		return;}
 	ob.price=document.getElementById("price").value;
-	if(ob.price.length==0)
+	if(ob.price.length==0 || isNaN(ob.price))
 	{unHideAddNewProductLink();
-      alert("price cant be left empty");
+      alert("price details not up to mark");
 		return;}
-	ob.quant=parseInt(document.getElementById("quant").value,10);
-	if(ob.price.length==0)
+	ob.quant=document.getElementById("quant").value;
+	if(ob.quant.length==0|| isNaN(ob.quant))
 	{unHideAddNewProductLink();
-      alert("quant cant be left empty");
+      alert("quantity details not up to the mark");
 		return;}
 	var f=pid;
 	pid++;
@@ -365,12 +365,12 @@ function upar(t,tp)
       alert("Description cant be left empty");
 		return;}
 	products[i].price=document.getElementById("price").value;
-	if(products[i].price.length==0 && parseInt(document.getElementById("price").value)==NaN)
+	if(products[i].price.length==0 && isNaN(products[i].price))
 	{unHideAddNewProductLink();
       alert("price cant be left empty");
 		return;}
 	products[i].quant=parseInt(document.getElementById("quant").value);
-	if(products[i].quant.length==0)
+	if(products[i].quant.length==0 || isNaN(products[i].quant))
 	{unHideAddNewProductLink();
       alert("Quantity cant be left empty");
 		return;}
@@ -392,7 +392,7 @@ function upar(t,tp)
 	 
 	 var jso1=JSON.stringify(cart);
      localStorage.setItem("cart",jso1);
-	 
+	                               
 	 updom(tp);
 }
 
@@ -564,5 +564,75 @@ function ndom(k)
 	
 	list.appendChild(pr);
 	unHideAddNewProductLink();
+}
+
+function itemQuant(tp)
+{
+	var f=document.getElementById(tp);
+	var par=f.childNodes[0];
+	var child=f.childNodes[0].childNodes[3];
+     var i=0;
+	for(i=0;i<products.length;i++)
+	{
+		if(products[i].id==tp){
+			break;
+		}
+	}
+	var l=products[i].quant;
+	
+
+     l=l-1;
+	 var x=document.createTextNode(l);
+	 console.log(x);
+	 par.replaceChild(x,child);
+	 products[i].quant=l;
+	 
+	 var jso=JSON.stringify(products);
+     localStorage.setItem("shopper",jso);
+	// var
+	
+}
+
+
+function addtocart(t)
+{
+	var i=0;
+	for(var i=0;i<products.length;i++)
+	{
+		if(products[i].id==t){
+			break;
+		}
+	}
+	
+    var s=new Object();
+	s.id=products[i].id;
+	s.name=products[i].name;
+	s.desc=products[i].desc;
+	s.price=products[i].price;
+	s.quant=1;
+	var l=products[i].quant;
+	if(l==0)
+	{
+		alert("ITEM IS OUT OF STOCK");
+		return ;
+	}
+	//alert(cart.length);
+	for(var  u=0;u<cart.length;u++)
+	{
+		if(cart[u].id==t)
+		{
+			cart[u].quant=cart[u].quant+1;
+			var jso=JSON.stringify(cart);
+            localStorage.setItem("cart",jso);
+			 itemQuant(t);
+			return;
+		}
+	}
+	cart.push(s);
+	var jso=JSON.stringify(cart);
+     localStorage.setItem("cart",jso);
+	 alert(s.name+" added to cart");
+	
+	 itemQuant(t);
 }
 
